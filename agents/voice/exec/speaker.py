7933365fc7d92f49a0190669ec24b9ea4e8be39a
deerlord <acidcatnoise@gmail.com>
2019-11-27 08:28:47 +0000

    updates

--- agents/voice/exec/speaker.py
+++ agents/voice/exec/speaker.py
@@ -1,29 +1,58 @@
-
-
+import sounddevice
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class AudioPlayer():
+    def __check_devices(self):
+        retval = False
+        devices = sd.query_devices()
+        try:
+            soundcard = [
+                d
+                for d in devices
+                if 'name' in d and d['name'] == 'dmix'
+            ][0]
+            if soundcard['max_output_channels'] < 0:
+                raise Exception
+        except IndexError:
+            # cannot find soundcard to use
+            return retval
+        except:
+            return retval
+        if len(devices) >= 3:
+            soundcard = devices[2]
+            soundcard['id'] = 2
+            self.__soundcard = soundcard
+            retval = True
+        return retval
+
     def __init__(self):
+        self.__check_devices()
         self.__stream = sounddevice.RawOutputStream(
             samplerate=44100,
             blocksize=1024,
-            device=2,
+            device=self.__soundcard['id'],
             channels=2,
         )
+
     def play(self, data):
         # make sure data is prepared for writing
         self.__stream.write(data)
-listener = AudioListener()
-# start thread
+
+
 player = AudioPlayer()
+
+
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         audio = self.rfile.read(content_length)
         self.send_response(200)
         self.end_headers()
-        listener.stop()
+        # stop listener
         player.play(audio)
-        listener.start()
+        # start listener
+
+
 httpd = HTTPServer(('', 8080), RequestHandler)
 httpd.serve_forever()
 
