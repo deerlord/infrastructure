dbba8eb32ebf378dc090a1b4d1d2372ed94219a7
deerlord <acidcatnoise@gmail.com>
2019-11-30 00:10:01 +0000

    updates

--- containers/22-command/app/__main__.py
+++ containers/22-command/app/__main__.py
@@ -1,25 +1,9 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from io import BytesIO
-from openhab import OpenHAB
+import openhabapi
 import language
 
 KEYWORD = 'HAL'.lower()
-base_url = 'http://openhab:8080/rest'
-hab = OpenHAB(base_url)
-
-
-def openhab_process(command):
-    """
-    # figure out wtf we are doing
-    if interact with item
-      items = hab.()
-      i = items.get(item_name)
-      if i
-       # might need to parse
-       i.command(cmd)
-    """
-    pass
-
 
 class SpeechHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -27,8 +11,17 @@ class SpeechHandler(BaseHTTPRequestHandler):
         audio = self.rfile.read(content_length)
         retval = False
         result = language.pipeline(audio)
+        # convert into command
+        """
+        command = {
+            'action': 'ON',
+            'name': 'light 1',
+            'plural': False,
+            'room': None,
+        }
+        """
         if result:
-            openhab_process(command)
+            openhab.request(command)
             retval = True
         self.send_response(200 if retval else 500)
         self.end_headers()
--- containers/22-command/app/openhabapi.py
+++ containers/22-command/app/openhabapi.py
@@ -1,28 +1,36 @@
 from openhab import OpenHAB
-import requests
 
-hab = OpenHab('http://openhab:8080/rest')
+hab = OpenHAB('http://openhab:8080/rest')
 
-# get items in room
-# get sets of items from room
+ACTIONS = [
+    'on',
+    'off',
+    'increase',
+    'decrease',
+    'up',
+    'down',
+    'stop',
+]
 
-class Room():
-    __items = {}
-    def __init__(self, name):
-        self.__name = name
-        self.__refresh_items()
+def request(command):
+    action = command['action'].lower()
+    if action not in ACTIONS:
+        return None
+    if command['plural']:
+        retval = __action_to_group(command['name'], action)
+    else:
+        item = hab.get_item(command['name'])
+        retval = __take_action(item, action)
+    return retval
 
-    def __refresh_items():
-        self.__items = {
-            item.name: item
-            for item in hab.fetch_all_items():
-            if 'item.<attr>' == self.__name:
-        }
 
-    def __all_items_command(type_, command):
-        retval = None
-        for name, item in self.__items.items():
-            if item.type == type_:
-                cmd = getattr(item, 'command')
-                result = cmd(command)
-        return result
+def __take_action(item, action):
+    return getattr(item, action)() if hasattr(item, action) else None
+
+
+def __action_to_group(type_, action):
+    return all([
+        __take_action(item, action)
+        for item in hab.fetch_all_items()
+        if item.type == type_
+    ])
