2e5b372d38863cb825b11c935fb2fc85160f825b
Trent Dierking <deerxlord@gmail.com>
2020-10-07 19:20:26 -0500

    stripped to networking daemons

--- bin/reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker stop $(docker ps -qa)
-docker rm $(docker ps -qa)
-docker rmi -f $(docker images | tail -n+2 | awk '{print $3}')
-docker volume rm $(docker volume ls | tail -n+2 | awk '{print $2}')
-docker network rm $(docker network ls | tail -n+2 | awk '{print $1,$2}' | grep -v bridge | grep -v host | grep -v none | cut -d' ' -f1)
--- containers/99-weather/app/test_influxdb.py
+++
@@ -1,8 +0,0 @@
-import interface
-
-client = interface.InfluxDB()
-client.connect()
-client.create_database()
-data = {'dt': 1595243443, 'sunrise': 1595243663, 'sunset': 1595296278}
-client.current(data)
-print(client.get_current())
